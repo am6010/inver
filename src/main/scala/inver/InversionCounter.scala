92f02f319e185b5fcd2bd702aebd25fd964c1309
am6010 <alexmilaios@gmail.com>
2015-01-26 23:49:48 +0000

    making the inversion function more generic with type parameters

--- src/main/scala/inver/InversionCounter.scala
+++ src/main/scala/inver/InversionCounter.scala
@@ -4,42 +4,55 @@ package inver
 import scala.io.Source
 
 /**
- * Created by amilaios on 1/25/15 at 12:29 PM.
+ * Created by Alexandros Milaios on 1/25/15 at 12:29 PM.
+ *
+ * Count Inversions Algorithm
+ *
+ * Complexity O(nlog(n))
+ *
  */
 object InversionCounter {
 
 
   def parseFileInput[T](fileName : String, toT : String => T) : List[T] = {
     val textList = Source.fromFile(fileName).getLines().toList
-    textList.map(toT(_))
+    textList.map(toT)
   }
 
-  def sortAndCount[T](input: List[T], comp : (T,T) => Boolean) : (List[T], Long)  = {
+  /**
+   * Divide Part of the Algorithm
+   *
+   */
+  def sortAndCount[T<%Ordered[T]](input: List[T]) : (List[T], Long)  = {
     input match {
       case List() => (List(), 0)
       case List(_) => (input, 0)
-      case l => {
+      case l =>
         val n = input.size
-        val left = sortAndCount(input.take(n/2), comp)
-        val right = sortAndCount(input.drop(n/2), comp)
-        val split = countSplitInv(left._1, right._1, (List(), 0), comp)
-          (split._1, left._2 + right._2 + split._2)
-      }
+        val left = sortAndCount(input.take(n/2))
+        val right = sortAndCount(input.drop(n/2))
+        val split = countSplitInv(left._1, right._1, (List(), 0))
+        (split._1, left._2 + right._2 + split._2)
     }
   }
 
-  def countSplitInv[T](left: List[T], right: List[T], acc : (List[T], Long), comp : (T,T) => Boolean) : (List[T], Long) = {
+
+  /**
+   * Combine Part of the Algorithm
+   *
+   */
+  def countSplitInv[T<%Ordered[T]](left: List[T], right: List[T], acc : (List[T], Long)) : (List[T], Long) = {
     (left, right) match {
-      case (x :: xs, y :: ys) if comp(x,y) => countSplitInv(xs, y :: ys, (x :: acc._1, acc._2), comp)
-      case (x :: xs, y :: ys) => countSplitInv(x :: xs , ys, (y :: acc._1, acc._2 + left.size), comp)
+      case (x :: xs, y :: ys) if x < y => countSplitInv(xs, y :: ys, (x :: acc._1, acc._2))
+      case (x :: xs, y :: ys) => countSplitInv(x :: xs , ys, (y :: acc._1, acc._2 + left.size))
       case _ => if(left.isEmpty) (acc._1.reverse ::: right, acc._2)
               else (acc._1.reverse ::: left, acc._2)
     }
   }
 
 
-  def count[T](input: List[T],  comp : (T,T) => Boolean) : Long = {
-      sortAndCount(input, comp)._2
+  def count[T<%Ordered[T]](input: List[T]) : Long = {
+      sortAndCount(input)._2
   }
 
 }
--- src/test/scala/inver/InverSuite.scala
+++ src/test/scala/inver/InverSuite.scala
@@ -4,16 +4,16 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
-import inver._
 
 /**
- * Created by amilaios on 1/25/15 at 12:23 PM.
+ * Created by Alexandros Milaios on 1/25/15 at 12:23 PM.
+ *
  */
 
 @RunWith(classOf[JUnitRunner])
 class InverSuite extends FunSuite {
   trait IntegerList {
-    val list1 =  InversionCounter.parseFileInput("src/test/resources/tc1", _.toInt);
+    val list1 =  InversionCounter.parseFileInput("src/test/resources/tc1", _.toInt)
   }
 
   test("Input list tc1") {
@@ -24,7 +24,7 @@ class InverSuite extends FunSuite {
 
   test("Test Count Split Inv") {
     new IntegerList {
-      val p = InversionCounter.countSplitInv[Int](List(1,3,5), List(2,4,6), (List(),0), _ < _)
+      val p = InversionCounter.countSplitInv[Int](List(1,3,5), List(2,4,6), (List(),0))
       assert(p._1 === List(1,2,3,4,5,6))
       assert(p._2 === 3)
     }
@@ -32,24 +32,24 @@ class InverSuite extends FunSuite {
 
   test("test tc1") {
     new IntegerList {
-      assert(InversionCounter.count[Int](list1, _ < _)===3)
+      assert(InversionCounter.count[Int](list1)===3)
     }
   }
 
   test("test tc2") {
     val l = InversionCounter.parseFileInput("src/test/resources/tc2", _.toInt)
-    assert(InversionCounter.count[Int](l, _ < _)===4)
+    assert(InversionCounter.count[Int](l)===4)
   }
 
 
   test("test tc3") {
     val l = InversionCounter.parseFileInput("src/test/resources/tc3", _.toInt)
-    assert(InversionCounter.count[Int](l, _ < _)===10)
+    assert(InversionCounter.count[Int](l)===10)
   }
 
   test("test tc4") {
     val l = InversionCounter.parseFileInput("src/test/resources/tc4", _.toInt)
-    assert(InversionCounter.count[Int](l, _ < _)===56)
+    assert(InversionCounter.count[Int](l)===56)
   }
 
   test("test tc 5") {
@@ -59,15 +59,15 @@ class InverSuite extends FunSuite {
       10, 26, 48, 3, 2, 15, 92, 11, 55, 63, 97, 43, 45, 81, 42, 95, 20, 25, 74, 24, 72,
       91, 35, 86, 19, 75, 58, 71, 47, 76, 59, 64, 93, 17, 50, 56, 94, 90, 89, 32, 37,
       34, 65, 1, 73, 41, 36, 57, 77, 30, 22, 13, 29, 38, 16, 88, 61, 31, 85, 33, 54 )
-    assert(InversionCounter.count[Int](l, _ < _)===2372)
+    assert(InversionCounter.count[Int](l)===2372)
   }
 
   test("final test") {
-    val s = System.nanoTime();
+    val s = System.nanoTime()
     val l = InversionCounter.parseFileInput("src/test/resources/IntegerArray.txt", _.toInt)
     val t = System.nanoTime()
     println(t -s)
-    assert( InversionCounter.count[Int](l, _ < _)  === 2407905288L)
+    assert( InversionCounter.count[Int](l)  === 2407905288L)
     val f = System.nanoTime()
     println(f -t)
   }
